cb5bd19aa173bb982e3fd156be672d492f2e369f
Ray Weitzenberg <ray@mechaneyes.com>
2019-12-15 01:56:28 -0500

    hotspots working

---
+++ app/scripts/main.js
@@ -0,0 +1,26 @@
+let timeLine = gsap.timeline({ repeat: -1 });
+
+timeLine.to(".dotOuter", {
+	duration: 2,
+	scale: 3,
+	transformOrigin: "50% 50%",
+	opacity: 0,
+	ease: "power1.out"
+});
+
+$(function() {
+	$(".dotInner").click(function() {
+		$(".hotInfo").removeClass("hotVisi");
+		$(this)
+			.siblings(".hotInfo")
+			.addClass("hotVisi");
+	});
+
+	$(".hotSpotter").click(function() {
+		$(".hotInfo").removeClass("hotVisi");
+	});
+
+	$(".hotClose").click(function() {
+		$(".hotInfo").removeClass("hotVisi");
+	});
+});
